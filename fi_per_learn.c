0faade56e21d8d74fa414cba8016cfe633fe898a
Balram <balram4327@gmail.com>
2019-09-10 09:50:03 +0530

    bug fixed

--- fi_per_learn.c
+++ fi_per_learn.c
@@ -3,7 +3,6 @@
 int main()
 {
     int n,i;
-    int yj;
     float alp;
 
     printf("Enter the number of neurons. \n");
@@ -17,18 +16,39 @@ int main()
 
     float x[n][i];
     float Y[i];
+    float yj[i];
 
     for(int j=0;j<i;j++)
         x[0][j] = 1; //bias input
 
-    for(int k=0;k<i;k++)
+    /*for(int k=0;k<i;k++)
     {
         for(int j=1;j<n;j++)
         {
             printf("Enter the %d th neural values of %d th input: ",j,k);
             scanf("%f",&x[j][k]);
         }
-    }
+    } */
+
+
+//Hardcoded Values
+    x[1][0] = 0.25;
+    x[2][0] = 0.358;
+    x[1][1] = 0.25;
+    x[2][1] = 0.471;
+    x[1][2] = 0.5;
+    x[2][2] = 0.353;
+    x[1][3] = 0.5;
+    x[2][3] = 0.647;
+    x[1][4] = 0.75;
+    x[2][4] = 0.765;
+    x[1][5] = 0.75;
+    x[2][5] = 0.882;
+    x[1][6] = 1;
+    x[2][6] = 0.705;
+    x[1][7] = 1;
+    x[2][7] = 1;
+
 
     for(int j=0;j<i;j++)
     {
@@ -45,30 +65,64 @@ int main()
     }
 
     float netj;
+    int flag = 0;
+    int count =0;
 
+    while(flag==0)
+    {
     for(int k=0;k<i;k++)
     {
         //printf("Y value: %f \n",Y[k]);
-        netj = 0;
-        for(int j=0;j<n;j++)
+        
+        for(int j=-1;j<n;j++)
         {
-            netj = netj + x[j][k]*w[i];
+            if(j==-1)
+            netj=0;
+            
+            else
+            netj = netj + (x[j][k]*w[j]);
 
-            printf("NetJ value: %f %d %d \n",netj,k,j);
-                
+        //    printf("NetJ value: %f\n",netj);      
         }
-        yj = (netj > 0)? 1 : 0;
-        printf("Yj value: %f Y value: %f \n",yj,Y[k]);
+        yj[k] = (netj > 0)? 1 : 0;
+        // printf("Yj value: %f Y value: %f \n",yj[k],Y[k]);
+
+        if(yj[k]==1&&Y[k]==0)
+        {
+            for(int j=0;j<n;j++)
+                {
+                    w[j] = w[j] - alp*x[j][k];
+                }
+        }
+
+        else if(yj[k]==0&&Y[k]==1)
+        {
+        for(int j=0;j<n;j++)
+                {
+                    w[j] = w[j] + alp*x[j][k];
+                }
+        }
     }
 
-    for(int k=0;k<i;k++)
+    flag=-1;
+    for(int p=0;p<i;p++)
     {
-        for(int j=0;j<n;j++)
-        {
-            if(yj==1&&Y[k]==0)
-            w[j] = w[j] - alp*x[j][k];
-            else if(yj==0&&Y[k]==1)
-            w[j] = w[j] + alp*x[j][k];
-        }
+        if(yj[p]!=Y[p])
+        flag = 0;    
+    }
+
+    count++;
+    }
+
+    printf("Number of iterations: %d \n",count);
+
+ for(int l=0;l<n;l++)
+    {
+        printf("W: %f \n",w[l]);
+    }
+
+    for(int l=0;l<i;l++)
+    {
+        printf("yj: %f Y: %f \n", yj[l],Y[l]);
     }
 }
